063358040462c09d5a0126d40e9c569a222639d7
Carlos Daniel <66672234+f4ll-py@users.noreply.github.com>
2020-10-20 20:30:15 -0300

    Refatoração no código
    Refatoração parcial no código e adaptação de algumas features para evitar erros futuros.

--- calculadora.js
+++ calculadora.js
@@ -4,37 +4,30 @@
 var valor;
 var soma;
 var resultado;
-var percent; 
-var valor1;
-var valor2; 
-function botao(num){ //funções que cada botão precisa ter ao ser clicado
-try { // apenas uma forma de ver se o programa esta funcionando, caso contrario ele retornar uma mensagem de erro
+// var percent; |
+// var valor1; |
+// var valor2; -> Variáveis futuras
 
-    valor = document.calc.visor.value += num; //valor recebe o documento externo das variaveis anteriores contatenando o num
-   
-  
-} catch (valor) {
-    console.error("Syntaxe Error");
-}
-    
 
+function botao(num) { //funções que cada botão precisa ter ao ser clicado
+    try { // apenas uma forma de ver se o programa esta funcionando, caso contrario ele retornar uma mensagem de erro
+        valor = document.calc.visor.value += num; //valor recebe o documento externo das variaveis anteriores contatenando o num
+    } catch (valor) {
+        console.error("Syntaxe Error");
+    }
 }
 
-    function reseta(){ // onde o botao C apagará tudo que foi digitado
+function resetar() { // onde o botao C apagará tudo que foi digitado
+    document.calc.visor.value = "";
+}
 
-        document.calc.visor.value = " "; 
-    }
-    function visor(){
-        document.calc.visor.value = setEditable(false);
-    }
+function visor() {
+    document.calc.visor.value = setEditable(true);
+}
 
-    function calcula(value){ // aqui onde será calculado as operações recebidas
-
-        resultado = eval(valor);
-     
-        // resultado recebera o mesmo valor que foi calculado
-        document.calc.visor.value = resultado.toLocaleString('pt-br');
-
-
-    }
+function calcular() { // aqui onde será calculado as operações recebidas
+    resultado = eval(valor);
+    // resultado recebera o mesmo valor que foi calculado
+    document.calc.visor.value = resultado.toLocaleString('pt-br');
+}
     
